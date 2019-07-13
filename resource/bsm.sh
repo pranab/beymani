db70defeb56f4865bffeb777c046ef33d96db949
Pranab Ghosh <pkghosh99@gmail.com>
2019-07-12 22:58:54 -0700

    state transition config and scsript

--- resource/bsm.sh
+++ resource/bsm.sh
@@ -3,6 +3,7 @@
 PROJECT_HOME=/Users/pranab/Projects
 JAR_NAME=$PROJECT_HOME/bin/beymani/uber-beymani-spark-1.0.jar
 CHOMBO_JAR_NAME=$PROJECT_HOME/bin/chombo/uber-chombo-spark-1.0.jar
+AVENIR_JAR_NAME=$PROJECT_HOME/bin/avenir/uber-avenir-spark-1.0.jar
 MASTER=spark://akash:7077
 
 case "$1" in
@@ -15,6 +16,17 @@ case "$1" in
 	rm -rf ./output/bsm/trans
 	$SPARK_HOME/bin/spark-submit --class $CLASS_NAME   \
 	--conf spark.ui.killEnabled=true --master $MASTER $CHOMBO_JAR_NAME  $INPUT $OUTPUT bsm.conf
+	rm -rf ./output/bsm/trans/_SUCCESS
+;;
+
+"stateTrans")
+	echo "running MarkovStateTransitionModel"
+	CLASS_NAME=org.avenir.spark.sequence.MarkovStateTransitionModel
+	INPUT=file:///Users/pranab/Projects/bin/beymani/output/bsm/trans/*
+	OUTPUT=file:///Users/pranab/Projects/bin/beymani/output/bsm/sttr
+	rm -rf ./output/bsm/sttr
+	$SPARK_HOME/bin/spark-submit --class $CLASS_NAME   \
+	--conf spark.ui.killEnabled=true --master $MASTER $AVENIR_JAR_NAME  $INPUT $OUTPUT bsm.conf
 ;;
 
 *) 
