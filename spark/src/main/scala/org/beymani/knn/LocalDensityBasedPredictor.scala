c05c484b1d85eec06758ac30e121c91e6291d412
Pranab Ghosh <pkghosh99@gmail.com>
2020-08-06 08:14:00 -0700

    lof wip

--- spark/src/main/scala/org/beymani/knn/LocalDensityBasedPredictor.scala
+++ spark/src/main/scala/org/beymani/knn/LocalDensityBasedPredictor.scala
@@ -19,13 +19,15 @@ package org.beymani.knn
 
 import scala.math.max
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.chombo.spark.common.GeneralUtility
 import org.chombo.spark.common.JobConfiguration
 import org.chombo.spark.common.Record
+import scala.collection.mutable.ArrayBuffer
 import org.chombo.util.BasicUtils
 
 /**
-* local neighborhood density based outlier predictor aks Local Outlier Factor
+* local neighborhood density based outlier predictor aka Local Outlier Factor
 * @param args
 * @return
 */
@@ -64,6 +66,7 @@ object LocalDensityBasedPredictor extends JobConfiguration with GeneralUtility {
 	     val items = BasicUtils.getTrimmedFields(line, fieldDelimIn)
 	     val recs = Array.ofDim[(Record, Record)](2)
 	     
+	     //seq ID added to key
 	     var key = Record(keyLen + 1)
 	     populateFields(items, keyLen, key)
 	     key.addLong(items(keyLen).toLong)	     
@@ -72,6 +75,7 @@ object LocalDensityBasedPredictor extends JobConfiguration with GeneralUtility {
 	     value.addDouble(items(keyLen+2).toDouble)
 	     recs(0) = (key, value)
 	     
+	     //seq ID added to key
 	     key = Record(keyLen + 1)
 	     populateFields(items, keyLen, key)
 	     key.addLong(items(keyLen + 1).toLong)	     
@@ -82,12 +86,13 @@ object LocalDensityBasedPredictor extends JobConfiguration with GeneralUtility {
 	     recs
 	   }).cache
 	   
+	   //nearest k neighbors
 	   val kNeighbors = pairDistance.groupByKey.map(v => {
 	     val key = v._1
 	     val keyStr = key.toString
 	     val values = v._2.toList.sortBy(v => v.getDouble(1)).take(neighborCount)
 	     (key, values)
-	   })
+	   }).cache
 	   
 	   //distance to kth nearest neighbor
 	   val knDist = kNeighbors.map(r => {
@@ -125,6 +130,61 @@ object LocalDensityBasedPredictor extends JobConfiguration with GeneralUtility {
 	   })
 	   
 	   //local reachability density
+	   val lrd = rDist.map(r => {
+	     val key = Record(keyLen + 1, r._1, 0, keyLen+1)
+	     val value = Record(2)
+	     value.addInt(1)
+	     value.addDouble(r._2.getDouble(0))
+	     (key, value)
+	   }).reduceByKey((v1, v2)  => {
+	     val v = Record(2)
+	     v.addInt(v1.getInt(0) + v2.getInt(0))
+	     v.addDouble(v1.getDouble(1) + v2.getDouble(1))
+	     v
+	   }).mapValues(r => {
+	     val den = 1.0 / (r.getDouble(1) / r.getInt(0))
+	     createDoubleFieldRec(den)
+	   })
+	   
+	   //flatten source and k neighbors
+	   def flatNeighbors(ne : RDD[(Record, List[Record])]) :  RDD[(Record, Record)] = {
+	     ne.flatMap(r => {
+	       val flNeighbors = ArrayBuffer[(Record, Record)]()
+	       val k = Record(r._1)
+	       val v = Record(2)
+	       v.addLong(r._1.getLong(keyLen))
+	       v.addDouble(0)
+	       val nr = (k,v)
+	       flNeighbors += nr
+	       r._2.foreach(n => {
+	         val k = Record(keyLen + 1, r._1, 0, keyLen)
+	         k.add(n.getLong(0))
+	         val v = Record(2)
+	         v.addLong(r._1.getLong(keyLen))
+	         v.addDouble(0)
+	         val nr = (k,v)
+	         flNeighbors += nr
+	       })
+	       flNeighbors
+	     })
+	   }
+	   
+	   //find lof
+	   val x = lrd.union(flatNeighbors(kNeighbors)).groupByKey.flatMap(r => {
+	     //populate soure and each neighbor with lrd
+	     val values = r._2.toArray
+	     val lrdArr = values.filter(v => v.size == 1)
+	     BasicUtils.assertCondition(lrdArr.length == 1, "record containing lrd should have size 1")
+	     val lrd = lrdArr(0).getDouble(0)
+	     val nValues = values.filter(v => v.size == 2).map(v => {
+	       v.addDouble(1, lrd)
+	       val k = Record(keyLen + 1, r._1, 0, keyLen)
+	       k.addLong(v.getLong(0))
+	       v.addLong(0, k.getLong(keyLen))
+	       (k, v)
+	     })
+	     nValues
+	   })
 	   
    } 
 }
